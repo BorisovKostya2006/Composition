a1d8e78bd8516d21b6be850c77b6f60d5921219d
Kostya Borisov <borisov.kostya999@gmail.com>
2025-04-02 22:50:10 +0300

    binding

--- app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.composition.R
+import com.example.composition.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
 
+    private  var _binding : FragmentWelcomeBinding? = null
+    private val binding : FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding==null")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +24,17 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
